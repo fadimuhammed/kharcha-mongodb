1908993de3341cd210728551d9f3d905da63510d
fadimuhammed <fadimuhammed.m@gmail.com>
2026-04-16 23:07:49 +0530

    fix: add HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-// frontend/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import { AuthProvider } from "./context/AuthContext";
+import { HashRouter } from "react-router-dom"; // ✅ ADD THIS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <HashRouter>   {/* ✅ ADD THIS */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
   </React.StrictMode>
 );
