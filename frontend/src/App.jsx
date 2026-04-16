4ec8007befa5a2c8fcb70d77c1b2568170437b0f
fadimuhammed <fadimuhammed.m@gmail.com>
2026-04-16 22:08:16 +0530

    frontend connected to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import { useAuth } from './context/AuthContext';
 import Layout        from './components/Layout';
 import LoginPage     from './pages/LoginPage';
